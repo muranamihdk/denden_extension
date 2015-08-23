4c4ffbeddb46cd49654592dd752b3037119a9bef
Hideaki Muranami <muranamihideaki@gmail.com>
2015-08-23 16:21:50 +0900

    add: denden_extension.py, setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+setup (
+    name='denden_extension',
+    version='0.1',
+    py_modules=['denden_extension'],
+    install_requires=['markdown>2.6'],
+)
+
